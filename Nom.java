efebc48c3808f84d8fa80cb4de5b16a382f6f283
Mohammed Oussama Chelly <mohammedoussamachelly-debug@github.com>
2026-04-22 21:28:28 +0100

    Add Nom and pretraiteur classes

---
+++ Nom.java
@@ -0,0 +1,28 @@
+public abstract class Nom {
+    public int id;
+ private string nom_original;  
+ private string nom_pretraité; 
+ private string source ;
+    public Nom(int id, string nom_original, string nom_pretraité, string source) {
+        this.id = id;
+        this.nom_original = nom_original;
+        this.nom_pretraité = nom_pretraité;
+        this.source = source;
+
+
+    }
+ public int getId() {
+        return id;
+    }
+
+    public string getNomOriginal() {
+        return nom_original;
+    }
+
+    public string getNomPretraite() {
+        return nom_pretraité;
+    }
+
+    public string getSource() {
+        return source;
+    }
---
+++ pretraiteur.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class pretraiteur {
+
+ public List<String> pretraiter(List<String> listeEntree){
+    return null;
+ }
+
+}
