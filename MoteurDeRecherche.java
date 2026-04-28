14dba8fbd928e7714e19a5d6b7a77195479227cc
mohamedraslenamdouni-source <mohamedraslen.amdouni@etudiant-enit.utm.tn>
2026-04-28 02:19:54 +0100

    Create MoteurDeRecherche class with basic structure

---
+++ MoteurDeRecherche.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MoteurDeRecherche {
+   private configuration config;
+   public MoteurDeRecherche(configuration config){
+       this.config = config;
+   }  
+  
+  public group chercher(string nom ,string[] a ){
+    return null;
+} 
+}
