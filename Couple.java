61496de81374d1cee55c6f8bc106f3c64b09b33d
Your Name <you@example.com>
2026-05-07 10:26:21 +0100

    new sumbission

--- Couple.java
+++ Couple.java
@@ -1,9 +1,15 @@
 public class Couple {
-    public Nom nom1;
-    public Nom nom2;
+    private Nom nom1;
+    private Nom nom2;
+    private double score;
 
-    public Couple(Nom nom1, Nom nom2) {
+    public Couple(Nom nom1, Nom nom2, double score) {
         this.nom1 = nom1;
         this.nom2 = nom2;
+        this.score = score;
     }
+
+    public Nom getNom1()    { return nom1; }
+    public Nom getNom2()    { return nom2; }
+    public double getScore() { return score; }
 }
