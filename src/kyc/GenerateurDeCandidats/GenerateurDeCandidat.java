2f33e555b806cc13d6db5fe041a6d352a3facd62
chelly <mohammedoussama.chelly@etudiant-enit.utm.tn>
2026-05-14 18:22:01 +0100

    Update GenerateurDeCandidat.java

--- src/kyc/GenerateurDeCandidats/GenerateurDeCandidat.java
+++ src/kyc/GenerateurDeCandidats/GenerateurDeCandidat.java
@@ -1,1 +1,10 @@
+package kyc.GenerateurDeCandidats;
 
+import kyc.indexation.Index;
+import kyc.model.Couple;
+import kyc.model.Nom;
+import java.util.List;
+
+public interface GenerateurDeCandidat {
+    List<Couple> generercandidat(List<Nom> requetes, List<Nom> candidats, Index index);
+}
