6f781014b2c4f0bcd4ac05166546b8afe132235b
mohamedraslenamdouni-source <mohamedraslen.amdouni@etudiant-enit.utm.tn>
2026-05-14 22:47:05 +0100

    Update IndexTri.java

--- src/kyc/indexation/IndexTri.java
+++ src/kyc/indexation/IndexTri.java
@@ -1,1 +1,22 @@
+package kyc.indexation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class IndexTri implements Index {
+
+    private List<String> noms = new ArrayList<>();
+
+    @Override
+    public void indexer(List<String> noms) {
+        this.noms = new ArrayList<>(noms);
+        Collections.sort(this.noms, String.CASE_INSENSITIVE_ORDER);
+    }
+
+    @Override
+    public List<String> getNoms() {
+        return noms;
+    }
+}
 
