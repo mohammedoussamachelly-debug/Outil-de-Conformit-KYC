819b8c808f6f74bcf98f6e915bb5897cb25fec4e
ghassenby1 <ghassen.benyakhlef@etudiant-enit.utm.tn>
2026-05-14 23:13:17 +0100

    Add accent removal functionality in SupprimerAccents
    Implement accent removal in names during preprocessing.

--- src/kyc/pretraitement/SupprimerAccents.java
+++ src/kyc/pretraitement/SupprimerAccents.java
@@ -1,1 +1,20 @@
+package kyc.pretraitement;
 
+import kyc.model.Nom;
+import java.util.List;
+
+public class SupprimerAccents extends Pretraiteur {
+
+    @Override
+    public List<String> pretraiter(Nom a) {
+        List<String> parts = a.getNomPretraite();
+        parts.replaceAll(s -> s
+            .replaceAll("[àâä]", "a")
+            .replaceAll("[éêëè]", "e")
+            .replaceAll("[ç]", "c")
+            .replaceAll("[ùûü]", "u")
+            .replaceAll("[îï]", "i")
+            .replaceAll("[ôö]", "o"));
+        return parts;
+    }
+}
