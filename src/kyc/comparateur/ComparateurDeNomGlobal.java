4ac804fa467ceda2c5576520111b96ab226f3a10
chelly <mohammedoussama.chelly@etudiant-enit.utm.tn>
2026-05-14 18:19:55 +0100

    Update ComparateurDeNomGlobal.java

--- src/kyc/comparateur/ComparateurDeNomGlobal.java
+++ src/kyc/comparateur/ComparateurDeNomGlobal.java
@@ -1,1 +1,17 @@
 
+package kyc.comparateur;
+
+import kyc.model.Nom;
+
+public class ComparateurDeNomGlobal implements ComparateurDeNom {
+
+    private final ComparateurJaroWinkler jw = new ComparateurJaroWinkler();
+    private final ComparateurLevenshtein lev = new ComparateurLevenshtein();
+
+    public double comparer(Nom a, Nom b) {
+        String strA = String.join(" ", a.getNomPretraite());
+        String strB = String.join(" ", b.getNomPretraite());
+        if (strA.isEmpty() || strB.isEmpty()) return 0.0;
+        return (jw.comparer(strA, strB) + lev.comparer(strA, strB)) / 2.0;
+    }
+}
