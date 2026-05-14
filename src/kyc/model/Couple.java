d17970f567c15ec3a4848c05940e612aba2955e0
ghassenby1 <ghassen.benyakhlef@etudiant-enit.utm.tn>
2026-05-14 23:10:52 +0100

    Add Couple class with properties and constructor

--- src/kyc/model/Couple.java
+++ src/kyc/model/Couple.java
@@ -1,1 +1,18 @@
+package kyc.model;
 
+public class Couple {
+
+    private Nom nom1;
+    private Nom nom2;
+    private double score;
+
+    public Couple(Nom nom1, Nom nom2, double score) {
+        this.nom1  = nom1;
+        this.nom2  = nom2;
+        this.score = score;
+    }
+
+    public Nom    getNom1()  { return nom1; }
+    public Nom    getNom2()  { return nom2; }
+    public double getScore() { return score; }
+}
