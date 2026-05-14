b9f46dbc1098c5127d0f77567d2e2c4a212a56bf
chelly <mohammedoussama.chelly@etudiant-enit.utm.tn>
2026-05-14 18:20:30 +0100

    Update ComparateurEgalitExact.java

--- src/kyc/comparateur/ComparateurEgalitExact.java
+++ src/kyc/comparateur/ComparateurEgalitExact.java
@@ -1,1 +1,9 @@
+package kyc.comparateur;
 
+public class ComparateurEgalitExact implements ComparateurDeChaine {
+
+    @Override
+    public double comparer(String a, String b) {
+        return a.equals(b) ? 1.0 : 0.0;
+    }
+}
