de39d8fd9723ab04d39ad3e2c387afd8a94c8063
ghassenby1 <ghassen.benyakhlef@etudiant-enit.utm.tn>
2026-05-14 23:09:23 +0100

    Add GroupeIndex class with basic functionality

--- src/kyc/model/GroupeIndex.java
+++ src/kyc/model/GroupeIndex.java
@@ -1,1 +1,22 @@
+package kyc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupeIndex {
+
+    private String cle;
+    private List<Nom> noms;
+
+    public GroupeIndex(String cle) {
+        this.cle  = cle;
+        this.noms = new ArrayList<>();
+    }
+
+    public void ajouter(Nom nom) {
+        noms.add(nom);
+    }
+
+    public String    getCle()  { return cle; }
+    public List<Nom> getNoms() { return noms; }
+}
