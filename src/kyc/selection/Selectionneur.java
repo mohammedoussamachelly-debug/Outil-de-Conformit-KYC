ac4fb569364f94c5b507e77a3bcaee4a2b123752
mohamedraslenamdouni-source <mohamedraslen.amdouni@etudiant-enit.utm.tn>
2026-05-14 18:26:43 +0100

    Update Selectionneur.java

--- src/kyc/selection/Selectionneur.java
+++ src/kyc/selection/Selectionneur.java
@@ -1,1 +1,10 @@
 
+package kyc.selection;
+
+import kyc.model.Couple;
+import kyc.model.Groupe;
+import java.util.List;
+
+public interface Selectionneur {
+    Groupe selectionner(List<Couple> candidats, double score);
+}
