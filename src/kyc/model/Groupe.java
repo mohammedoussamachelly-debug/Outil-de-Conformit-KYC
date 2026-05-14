e3fd8447529d4af0beff6e7977d91eb2e5f02cce
ghassenby1 <ghassen.benyakhlef@etudiant-enit.utm.tn>
2026-05-14 23:10:28 +0100

    Add Groupe class with score and couples management

--- src/kyc/model/Groupe.java
+++ src/kyc/model/Groupe.java
@@ -1,1 +1,22 @@
+package kyc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Groupe {
+
+    private double score;
+    private List<Couple> couples;
+
+    public Groupe(double score) {
+        this.score   = score;
+        this.couples = new ArrayList<>();
+    }
+
+    public void ajouter(Couple couple) {
+        couples.add(couple);
+    }
+
+    public double       getScore()  { return score; }
+    public List<Couple> getCouples(){ return couples; }
+}
