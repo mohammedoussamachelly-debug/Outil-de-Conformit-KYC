79524c4e23cdcfe1140e963346b7008e489f79a2
ghassenby1 <ghassen.benyakhlef@etudiant-enit.utm.tn>
2026-05-14 23:12:24 +0100

    Implement ConvertirMinuscule to convert names to lowercase

--- src/kyc/pretraitement/ConvertirMinuscule.java
+++ src/kyc/pretraitement/ConvertirMinuscule.java
@@ -1,1 +1,14 @@
+package kyc.pretraitement;
 
+import kyc.model.Nom;
+import java.util.List;
+
+public class ConvertirMinuscule extends Pretraiteur {
+
+    @Override
+    public List<String> pretraiter(Nom a) {
+        List<String> parts = a.getNomPretraite();
+        parts.replaceAll(String::toLowerCase);
+        return parts;
+    }
+}
